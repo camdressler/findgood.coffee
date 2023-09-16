01e336b124a80b1a71e4a742276027cd384e8f4a
Cam <cameronsdressler@gmail.com>
2023-09-16 16:37:53 -0700

    Delete constants.ts

--- apps/web/lib/constants.ts
+++
@@ -1,4 +0,0 @@
-export const fullSiteName = "Find Good Coffee"
-export const siteName = "FindGoodCoffee"
-export const domain = "findgood.coffee"
-export const fullDomain = "https://findgood.coffee/"
