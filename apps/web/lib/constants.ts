f25069a1e6f9a41c1bafc58eb3f139f45314cbe7
Cam <cameronsdressler@gmail.com>
2023-09-16 16:15:52 -0700

    Create constants.ts

---
+++ apps/web/lib/constants.ts
@@ -0,0 +1,4 @@
+export const fullSiteName = "Find Good Coffee"
+export const siteName = "FindGoodCoffee"
+export const domain = "findgood.coffee"
+export const fullDomain = "https://findgood.coffee/"
