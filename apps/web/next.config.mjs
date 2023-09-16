bbd84948207c0332e367f8ef25a3b623d0880d0b
Cam <cameronsdressler@gmail.com>
2023-09-16 16:37:50 -0700

    Update next.config.mjs

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+        serverComponentsExternalPackages: ["@prisma/client"]
+    }
+};
+
+export default nextConfig;
